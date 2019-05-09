ed631bc01620ae923e2c1fb6dfc6b67ed0ef0fdf
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-09 10:00:25 +0200

    Projektdateien hinzufügen.

---
+++ Donnerstag/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Programm zum Donnerstag in Woche 2" << endl;
+
+	int Jahr = 2019;
+
+	int Zahlen[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	double Messwerte[10] = {0.1, 0.4, 0.6, 1.9, 2.0, 3.0, 5.0, 0.3,0.7,0.8};
+
+	string Monate[12] = {
+		"Januar","Februar", "Maerz","April",
+		"Mai","Juni", "Juli", "August","September",
+		"Oktober","November", "Dezember"};
+
+
+	// Neue Variable, dem der Inhalt "Januar" zugewiesen wird
+	string EinMonat = Monate[0];
+
+	// Ausgabe des ertsen Buchstaben also hier "J"
+	cout << EinMonat[0] << endl;
+
+	cout << Monate[1] << endl;
+	Monate[1] = "Karnevalsmonat :p";
+	cout << Monate[1] << endl;
+
+
+	cout << "Name des letzten Monats: " << Monate[11] << endl;
+	system("pause");
+	return 0;
+}
